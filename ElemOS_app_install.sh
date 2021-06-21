7287cd072fe9592ded46b74693162a52f9d6cdf5
MCKidL (Lenni) <81872657+ldstabler@users.noreply.github.com>
2021-06-21 14:35:06 -0400

    Add files via upload

---
+++ ElemOS_app_install.sh
@@ -0,0 +1,18 @@
+echo Installing GNOME system monitor [sudo apt-get install gnome-system-monitor]
+sudo apt-get install gnome-system-monitor
+echo Installing Nautilus file manager [sudo apt-get install nautilus]
+sudo apt-get install nautilus
+echo Installing GNOME Calendar [sudo apt-get install gnome-calendar]
+sudo apt-get install gnome-calendar
+echo Installing GNOME contacts [sudo apt-get install gnome-contacts]
+sudo apt-get install gnome-contacts
+echo Installing Thunderbird [sudo apt-get install thunderbird
+sudo apt-get install thunderbird
+echo Installing GIMP [sudo apt-get install gimp]
+sudo apt-get install gimp
+echo Installing Chromium [sudo apt-get install chromium]
+sudo apt-get install chromium
+echo Installing LibreOffice [sudo apt-get install libreoffice]
+sudo apt-get install libreoffice
+echo Update and upgrade time! [sudo apt-get update && sudo apt-get upgrade]
+sudo apt-get update && sudo apt-get upgrade
